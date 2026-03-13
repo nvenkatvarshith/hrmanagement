72c3db37a254c845c71046539d51ff1492ceb4ef
nvenkatvarshith <venkatvarshith2@gmail.com>
2026-03-13 10:43:58 +0530

    added user login and register validation using react hook form

--- app/(auth)/User/Login/page.tsx
+++ app/(auth)/User/Login/page.tsx
@@ -1,6 +1,21 @@
+"use client"
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+interface FormValues{
+    email: string;
+    password: string;
+    rememberme: boolean;
+}
+
 function Login(){
-    
+    const {register, handleSubmit, formState:{errors}} = useForm<FormValues>();
+
+    const validateUser = (formValues:FormValues) => {
+        console.log(formValues);
+        //Api call to validate user
+        window.location.href = "/User/Dashboard";
+    };
+
     return (
         <div className="h-screen flex">
             <div className="left-login basis-1/3">
@@ -8,23 +23,36 @@ function Login(){
                 <div className="px-10">
                     <h1 className='text-4xl mt-8 text-navyblue'>Login</h1>
                     <h3 className='text-2xl mt-3 text-gray-500'>Login to your account</h3>
-                    <form className="mt-4">
+                    <form className="mt-4" onSubmit={handleSubmit(validateUser)}>
                         <div>
                             <label htmlFor="email" className="text-navyblue">E-mail address</label><br />
-                            <input type="email" id='email' className='w-full my-2 py-1 border border-gray-500'/>
+                            <input type="email" id='email' className='w-full my-2 py-1 border border-gray-500'
+                                {...register('email',{
+                                    required: {value: true, message: "Email is required"},
+                                    pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/, message: 'Incorrect email format'} 
+                                })}/>
+                            {errors.email?.message && <p className="text-sm text-red-600 mb-0">{String(errors.email.message)}</p>}
                         </div>
                         <div className="mt-3">
                             <label htmlFor="password" className="text-navyblue">Password</label> <br />
-                            <input type="password" id='password' className='w-full my-2 py-1 border border-gray-500'/>
+                            <input type="password" id='password' className='w-full my-2 py-1 border border-gray-500'
+                                {...register('password',{
+                                    required: {value:true, message: "Password is required"},
+                                    pattern: {value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, message: "Use 8+ chars with uppercase, lowercase, digit and special character (!@#$%^&*)"}
+                                })}
+                            />
+                            {errors.password?.message && <p className="text-sm text-red-600 mb-0">{String(errors.password.message)}</p>}
                         </div>
                         <div className='flex justify-between w-full my-2'>
                             <div>
-                                <input type="checkbox" name="rememberme" id="rememberme" className='me-1'/>
+                                <input type="checkbox"  id="rememberme" className='me-1'
+                                 {...register('rememberme') }
+                                />
                                 <label htmlFor="rememberme" className="text-gray-500">Remember me</label>
                             </div>
-                            <a href="#" className="text-navyblue">Reset Password?</a>
+                            <Link href="#" className="text-navyblue">Reset Password?</Link>
                         </div>
-                        <button className='py-2 mt-3 w-full text-white bg-navyblue'>Sign In</button>
+                        <button className='py-2 mt-3 w-full text-white bg-navyblue' type="submit">Sign In</button>
                         <p className="mt-3 text-center text-gray-500">Don’t have an account yet? <Link href="/User/Register" className="text-navyblue">Join KRIS today.</Link></p>
                         
                     </form>
--- app/(auth)/User/Register/page.tsx
+++ app/(auth)/User/Register/page.tsx
@@ -1,7 +1,25 @@
-'use client'
+"use client"
 import Link from "next/link";
-function Register(){
+import {useForm} from "react-hook-form"
+interface RegisterForm{
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneno: string;
+    password: string;
+    confirmpassword: string;
+    newsletter: string;
+    policies: boolean;
+}
 
+function Register(){
+    const {register, handleSubmit, formState:{errors}} = useForm<RegisterForm>();
+    const registerUser = (formValues: RegisterForm) => {
+        console.log(formValues);
+        if(formValues.policies === true){
+            window.location.href = "/User/Login"
+        }
+    }
     return (
         <div className="h-screen flex">
             <div className="left bg-[url('/User/Register/register_user.jpg')] bg-cover bg-center basis-1/3">
@@ -24,42 +42,78 @@ function Register(){
                 <h1 className="text-6xl">Welcome to KRIS</h1>
                 <p className="text-gray-500 mt-2 text-2xl">Register your account</p>
                 <div className="user-register mt-4 w-3xl">
-                    <form>
+                    <form onSubmit={handleSubmit(registerUser)}>
                         <div className="flex gap-4">
                             <div className="basis-1/2">
                                 <label htmlFor="firstname" className="text-navyblue">First Name</label> <br />
-                                <input type="text" name="firstName" id="firstname" className="border rounded-sm w-full h-8" />
+                                <input type="text" id="firstname" className="border rounded-sm w-full h-8" 
+                                    {...register('firstName',{
+                                        required: {value: true, message: "FirstName is required"},
+                                        pattern: {value: /^[A-Za-z]+$/, message: "Firstname should be a word(only alphabets)"}
+                                    })}
+                                />
+                                {errors.firstName?.message && <p className="text-sm text-red-600 mb-0">{String(errors.firstName.message)}</p>}
                             </div>
                             <div className="basis-1/2">
                                 <label htmlFor="lastname" className="text-navyblue">Last Name</label> <br />
-                                <input type="text" name="lastName" id="lastname" className="border rounded-sm w-full h-8" />
+                                <input type="text" id="lastname" className="border rounded-sm w-full h-8" 
+                                {...register('lastName',{
+                                        required: {value: true, message: "Last Name is required"},
+                                        pattern: {value: /^[A-Za-z]+$/, message: "Last Name should be a word(only alphabets)"}
+                                    })}/>
+                                {errors.lastName?.message && <p className="text-sm text-red-600 mb-0">{String(errors.lastName.message)}</p>}
+                        
                             </div>
                         </div>
                         <div className="flex gap-4 mt-5">
                             <div className="basis-1/2">
                                 <label htmlFor="email" className="text-navyblue">E-mail Address</label> <br />
-                                <input type="email" name="email" id="email" className="border rounded-sm w-full h-8" />
+                                <input type="email" id="email" className="border rounded-sm w-full h-8" 
+                                {...register('email',{
+                                    required: {value: true, message: "Email is required"},
+                                    pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/, message: 'Incorrect email format'} 
+                                })}/>
+                                {errors.email?.message && <p className="text-sm text-red-600 mb-0">{String(errors.email.message)}</p>}
                             </div>
                             <div className="basis-1/2">
                                 <label htmlFor="phoneno" className="text-navyblue">Phone Number</label> <br />
-                                <input type="number" name="phoneno" id="phoneno" className="border rounded-sm w-full h-8" />
+                                <input type="number" id="phoneno" className="border rounded-sm w-full h-8"
+                                {...register('phoneno',{
+                                    required: {value: true, message: "Phone Number is required"},
+                                    pattern: { value: /^(?:\+91|91|0)?[6-9]\d{9}$/, message: 'Incorrect Phone Number'} 
+                                })}/>
+                                {errors.phoneno?.message && <p className="text-sm text-red-600 mb-0">{String(errors.phoneno.message)}</p>}
                             </div>
                         </div>
                         <div className="flex gap-4 mt-5">
                             <div className="basis-1/2">
                                 <label htmlFor="password" className="text-navyblue">Password</label> <br />
-                                <input type="password" name="password" id="password" className="border rounded-sm w-full h-8" />
+                                <input type="password" id="password" className="border rounded-sm w-full h-8"
+                                {...register('password',{
+                                    required: {value:true, message: "Password is required"},
+                                    pattern: {value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, message: "Use 8+ chars with uppercase, lowercase, digit and special character (!@#$%^&*)"}
+                                })}
+                                />
+                                {errors.password?.message && <p className="text-sm text-red-600 mb-0">{String(errors.password.message)}</p>}    
                             </div>
                             <div className="basis-1/2">
                                 <label htmlFor="confirmpassword" className="text-navyblue">Confirm Password</label> <br />
-                                <input type="password" name="confirmpassword" id="confirmpassword" className="border rounded-sm w-full h-8" />
+                                <input type="password" id="confirmpassword" className="border rounded-sm w-full h-8" 
+                                {...register('confirmpassword',{
+                                    required: {value:true, message: "Confirm Password is required"},
+                                    pattern: {value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, message: "Use 8+ chars with uppercase, lowercase, digit and special character (!@#$%^&*)"}
+                                })}
+                                />
+                                {errors.confirmpassword?.message && <p className="text-sm text-red-600 mb-0">{String(errors.confirmpassword.message)}</p>}   
                             </div>
                         </div>
                         <div className="mt-5 text-gray-500">
-                            <input type="checkbox" name="newsletter" id="newsletter" /> Yes, I want to receive KRIS newsletters
-                            <div className="mt-2"><input type="checkbox" name="policies" id="policies" /> I agree to all the Terms, Privacy Policy</div>
+                            <input type="checkbox" id="newsletter" 
+                            {...register('newsletter',{})}
+                            /> Yes, I want to receive KRIS newsletters
+                            <div className="mt-2"><input type="checkbox" id="policies" {...register('policies',{})} /> I agree to all the Terms, Privacy Policy</div>
                         </div>
-                        <button className="border rounded-md bg-navyblue text-white py-3 px-9 mt-5">Create Account</button>
+                        <button className="border rounded-md bg-navyblue text-white py-3 px-9 mt-5" type="submit">Create Account</button>
                         <p className="mt-7">Already have an account? <Link href="/User/Login" className="text-navyblue">Log In</Link></p>
                     </form>
                 </div>
